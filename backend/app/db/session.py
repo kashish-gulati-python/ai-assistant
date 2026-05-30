6d8b0147a8e2747634322750f5c3140ee0952a74
Kashish Gulati <kgulati100@gmail.com>
2026-05-30 16:30:56 +0530

    feat: v0.1: Fixed a local DB connection bug where .env pointed to the Docker service name instead of localhost.

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 POSTGRES_DB_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:password@localhost:5432/ai-assistant-db")
-
+print(POSTGRES_DB_URL)
 engine = create_engine(url=POSTGRES_DB_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False)

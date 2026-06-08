34e3a2a92bd8a6bcc0c36bab2b33015b24c3bf93
Kashish Gulati <kgulati100@gmail.com>
2026-06-08 23:19:41 +0530

    Use server_default for the DB timestamp

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, func
 from app.db.base import Base
-from datetime import datetime, UTC
+from datetime import datetime
 import uuid
 
 class Users(Base):
@@ -11,17 +11,17 @@ class Users(Base):
     name: Mapped[str] = mapped_column(String)
     email = Column(String, unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
-    updated_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
 class Conversations(Base):
     __tablename__ = "conversations"
     user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     id:  Mapped[UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4)
     title: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
-    updated_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
-    deleted_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
 class Messages(Base):
     __tablename__ = "messages"
@@ -29,6 +29,6 @@ class Messages(Base):
     id:  Mapped[UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4)
     role: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
-    updated_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
-    deleted_at: Mapped[str] = mapped_column(DateTime, default=datetime.now(UTC))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
